8f0593d7278e7c95a1a65a85f4075f266557071c
jkchou0306 <jkchou0306@gmail.com>
2020-12-18 12:26:54 +0800

    修改了实验四

--- src/main/java/nefu/loginFilter.java
+++ src/main/java/nefu/loginFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = "/*",filterName = "loginFilter")
+@WebFilter(urlPatterns = "/index.jsp",filterName = "loginFilter")
 public class loginFilter implements Filter {
     protected FilterConfig filterConfig;
     public void destroy() {
